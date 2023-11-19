621afe752ae9e2d7db7002e03b9c1f381e004657
m4rc310RCP <marcelo.utfpr@me.com>
2023-11-18 23:02:54 -0300

    Deploy to dokku in ~>

--- src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
+++ src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
@@ -1,8 +1,11 @@
 package br.com.m4rc310.auth.graphql.services;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import br.com.m4rc310.auth.graphql.MConst;
+import foundation.cmo.opensales.graphql.mappers.annotations.MDate;
 import foundation.cmo.opensales.graphql.security.MAuth;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
@@ -11,10 +14,11 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @GraphQLApi
 public class AuthService implements MConst {
 	
+	@MDate
 	@MAuth(rolesRequired = "CLIENT")
 	@GraphQLQuery(name = "test")
-	public String test() {
-		return "OK";
+	public Date test() {
+		return new Date();
 	}
 	
 }
