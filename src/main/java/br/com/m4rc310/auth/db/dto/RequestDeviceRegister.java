6f489dcbdee81fc71dda75b4958077d0a309ae4b
m4rc310RCP <marcelo.utfpr@me.com>
2023-11-27 17:27:02 -0300

    Deploy to dokku in ~>

---
+++ src/main/java/br/com/m4rc310/auth/db/dto/RequestDeviceRegister.java
@@ -0,0 +1,23 @@
+package br.com.m4rc310.auth.db.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.com.m4rc310.auth.graphql.MConst;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.annotations.types.GraphQLType;
+import lombok.Data;
+
+@Data 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@GraphQLType(name = MConst.DTO$request_device_register)
+public class RequestDeviceRegister implements MConst {
+	@GraphQLQuery(name = CODE$request)
+	@JsonProperty("request_code")
+	public String code;
+	
+	@GraphQLQuery(name = IDENTIFY$device)
+	@JsonProperty("device_id")
+	public String deviceId;
+	
+}
--- src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
+++ src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
@@ -1,25 +1,31 @@
 package br.com.m4rc310.auth.graphql.services;
 
 import java.util.Date;
+import java.util.Random;
+import java.util.stream.Collectors;
 
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
 
+import br.com.m4rc310.auth.db.dto.RequestDeviceRegister;
 import br.com.m4rc310.auth.db.models.User;
 import br.com.m4rc310.auth.graphql.MConst;
 import foundation.cmo.opensales.graphql.mappers.annotations.MDate;
 import foundation.cmo.opensales.graphql.security.MAuth;
-import foundation.cmo.opensales.graphql.security.MAuthToken;
 import foundation.cmo.opensales.graphql.security.dto.MUser;
 import foundation.cmo.opensales.graphql.security.dto.MUserDetails;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.annotations.GraphQLSubscription;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
 @GraphQLApi
 public class AuthService extends MService implements MConst {
+	
+	private static final String FLUX_REGISTER_DEVICE = "flux_gegister_device";
 	
 	@MDate
 	@MAuth(rolesRequired = "CLIENT")
@@ -46,11 +52,31 @@ public class AuthService extends MService implements MConst {
 		return user;
 	}
 	
-	@GraphQLQuery(name = "${code.access.token}")
+	@GraphQLQuery(name = CODE$access_token, description = DESC$code_access_token)
 	public String getAccessToken(@GraphQLContext User user) {
 		MUser u = new MUser();
 		u.setUsername(user.getUsername());
 		return jwt.generateToken(MUserDetails.from(u));
 	}
+	
+	@GraphQLSubscription(name = SUBSCRIPTION$device_register)
+	public Publisher<RequestDeviceRegister> requestRegisterDevice() {
+		String code = getRandomNumber(5);
+		String id = String.format("%s_%s", FLUX_REGISTER_DEVICE, code);
+		
+		RequestDeviceRegister resp = new RequestDeviceRegister();
+		resp.setCode(code);
+		
+		return fluxService.publish(RequestDeviceRegister.class, id, resp);
+	}
+	
+	private String getRandomNumber(int length) {
+        return new Random()
+                .ints(0, 10) 
+                .limit(length) 
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining());
+    }
+	
 	
 }
