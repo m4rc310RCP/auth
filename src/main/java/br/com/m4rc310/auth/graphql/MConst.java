e596ec47a998a47f59289eedd2e7a18a6258ca79
m4rc310RCP <marcelo.utfpr@me.com>
2023-12-02 21:21:36 -0300

    Deploy to dokku in ~>

--- src/main/java/br/com/m4rc310/auth/graphql/MConst.java
+++ src/main/java/br/com/m4rc310/auth/graphql/MConst.java
@@ -14,6 +14,7 @@ public interface MConst {
 	public static final String DESC$description_password             = "${desc.description.password}";
 	public static final String DESC$dto_request_device_register      = "${desc.dto.request.device.register}";
 	public static final String DESC$identify_device                  = "${desc.identify.device}";
+	public static final String DESC$list_roles                       = "${desc.list.roles}";
 	public static final String DESC$mutation_auth_user               = "${desc.mutation.auth.user}";
 	public static final String DESC$mutation_confirm_register_device = "${desc.mutation.confirm.register.device}";
 	public static final String DESC$name_username                    = "${desc.name.username}";
@@ -22,13 +23,14 @@ public interface MConst {
 	public static final String DESCRIPTION$password                  = "${description.password}";
 	public static final String DTO$request_device_register           = "${dto.request.device.register}";
 	public static final String ERROR$access_unauthorized             = "${error.access.unauthorized}";
+	public static final String ERROR$invalid_code_register           = "${error.invalid.code.register}";
 	public static final String IDENTIFY$device                       = "${identify.device}";
+	public static final String LIST$roles                            = "${list.roles}";
 	public static final String MUTATION$auth_user                    = "${mutation.auth.user}";
 	public static final String MUTATION$confirm_register_device      = "${mutation.confirm.register.device}";
 	public static final String NAME$username                         = "${name.username}";
 	public static final String SUBSCRIPTION$device_register          = "${subscription.device.register}";
 	public static final String TYPE$auth_user                        = "${type.auth.user}";
 	public static final String TYPE$registry                         = "${type.registry}";
-	public static final String ERROR$invalid_code_register  = "${error.invalid.code.register}";
 }
 
--- src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
+++ src/main/java/br/com/m4rc310/auth/graphql/services/AuthService.java
@@ -3,6 +3,8 @@ package br.com.m4rc310.auth.graphql.services;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.reactivestreams.Publisher;
@@ -23,37 +25,73 @@ import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.annotations.GraphQLSubscription;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @GraphQLApi
 @EnableCaching
 public class AuthService extends MService implements MConst {
-	
+
 	private static final String FLUX_REGISTER_DEVICE = "flux_gegister_device";
-	
+
 	@MDate
-	//@MAuth(rolesRequired = "CLIENT")
+	// @MAuth(rolesRequired = "CLIENT")
 	@GraphQLQuery(name = "test")
 	public Date test() {
 		return new Date();
 	}
-	
+
 //	@MAuth(rolesRequired = "CLIENT")
 	@GraphQLMutation(name = MUTATION$auth_user)
-	public User authUser(@GraphQLArgument(name = "${code.hash}") String hash) throws Exception {
-		hash = jwt.decrypt(hash);
-		hash = hash.replace("\"", "");
-		
-		int i = hash.indexOf(":");
-		String username = hash.substring(0, i);
-		String password = hash.substring(i+1);
-		
-		
-		User user = new User();
-		user.setUsername(username);
-		user.setPassword(password);
-		
-		return user;
+	public User authUser(@GraphQLArgument(name = CODE$hash) String hash) throws Exception {
+		try {
+
+			hash = jwt.decrypt(hash);
+			hash = hash.replace("\"", "");
+
+			int i = hash.indexOf(":");
+			String username = hash.substring(0, i);
+
+			log.info(username);
+
+//			Pattern pattern = Pattern.compile("\\[([a-fA-F0-9\\-]+)\\]");
+//			Matcher matcher = pattern.matcher(username);
+//
+//			String key = "";
+//
+//			if (matcher.find()) {
+//				key = matcher.group(1);
+//			}
+//
+//			username = username.replace("[" + key + "]", "");
+//			log.info(key);
+
+			String password = hash.substring(i + 1);
+			password = jwt.decrypt(password);
+			
+			log.info(password);
+
+			// password = jwt.decrypt(password);
+
+			// log.info(password);
+
+			User user = new User();
+			user.setUsername(username);
+			user.setPassword(password);
+
+			return user;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+
+	@GraphQLQuery(name = "${list.roles}")
+	public String[] getRoles(@GraphQLContext User user) {
+		return new String[] {"ADMIN"};
 	}
 	
 	@GraphQLQuery(name = CODE$access_token, description = DESC$code_access_token)
@@ -62,9 +100,9 @@ public class AuthService extends MService implements MConst {
 		u.setUsername(user.getUsername());
 		return jwt.generateToken(MUserDetails.from(u));
 	}
-	
-	//=========================
-	
+
+	// =========================
+
 	@GraphQLSubscription(name = SUBSCRIPTION$device_register)
 	public Publisher<RequestDeviceRegister> requestRegisterDevice() {
 		String code = getRandomNumber(5);
@@ -72,36 +110,37 @@ public class AuthService extends MService implements MConst {
 		RequestDeviceRegister resp = getRequestDeviceRegister(code);
 		return fluxService.publish(RequestDeviceRegister.class, id, resp);
 	}
-	
+
 	@GraphQLQuery(name = NAME$username)
 	public String getUsernameRegister(@GraphQLContext RequestDeviceRegister rdr) {
 		try {
 			User user = rdr.getUserRegister();
-			return user.getUsername();			
+			return user.getUsername();
 		} catch (Exception e) {
 			return "";
 		}
 	}
-	
+
 	@GraphQLMutation(name = MUTATION$confirm_register_device)
-	public RequestDeviceRegister confirmRequestDeviceRegister(@GraphQLArgument(name = CODE$request) String code) throws Exception {
-		
+	public RequestDeviceRegister confirmRequestDeviceRegister(@GraphQLArgument(name = CODE$request) String code)
+			throws Exception {
+
 		String id = String.format("%s_%s", FLUX_REGISTER_DEVICE, code);
-		
+
 		if (fluxService.inPublish(RequestDeviceRegister.class, id)) {
 			RequestDeviceRegister register = getRequestDeviceRegister(code);
-			
+
 			String uuid = UUID.randomUUID().toString();
 			register.setDeviceId(uuid);
-			
+
 			fluxService.callPublish(RequestDeviceRegister.class, id, register);
 			resetCache(code);
 			return register;
 		}
-		
+
 		throw getException(ERROR$invalid_code_register, code);
 	}
-	
+
 	@Cacheable(value = "device_request", key = "#code")
 	private RequestDeviceRegister getRequestDeviceRegister(String code) {
 		RequestDeviceRegister resp = new RequestDeviceRegister();
@@ -109,19 +148,15 @@ public class AuthService extends MService implements MConst {
 		resp.setUserRegister(userAuth());
 		return resp;
 	}
-	
+
 	@CacheEvict(value = "device_request", key = "#code")
-	private void resetCache(String code) {}
-	
-	
+	private void resetCache(String code) {
+	}
+
 	private String getRandomNumber(int length) {
-        return new Random()
-                .ints(0, 10) 
-                .limit(length) 
-                .mapToObj(Integer::toString)
-                .collect(Collectors.joining());
-    }
-	
-	//=========================
-	
+		return new Random().ints(0, 10).limit(length).mapToObj(Integer::toString).collect(Collectors.joining());
+	}
+
+	// =========================
+
 }
