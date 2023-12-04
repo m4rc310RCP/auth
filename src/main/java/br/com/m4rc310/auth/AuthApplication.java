d5eaec8adaec21b7692ed896707df33a9f829740
m4rc310RCP <marcelo.utfpr@me.com>
2023-12-04 20:35:08 -0300

    Deploy to dokku in ~>

--- src/main/java/br/com/m4rc310/auth/AuthApplication.java
+++ src/main/java/br/com/m4rc310/auth/AuthApplication.java
@@ -9,35 +9,21 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-/**
- * The Class AuthApplication.
- */
+
 @SpringBootApplication
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:/security.properties")
 public class AuthApplication {
+	
 
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 	
-	/**
-	 * Inits the.
-	 */
 	@Bean
 	void init() {
 	    TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
 	}
 	
-	/**
-	 * B crypt password encoder.
-	 *
-	 * @return the password encoder
-	 */
 	@Bean
 	PasswordEncoder bCryptPasswordEncoder() {
 	    return new BCryptPasswordEncoder();
--- src/main/java/br/com/m4rc310/auth/graphql/MConst.java
+++ src/main/java/br/com/m4rc310/auth/graphql/MConst.java
@@ -1,99 +1,36 @@
 package br.com.m4rc310.auth.graphql;
 
 
-/**
- * The Interface MConst.
- */
 public interface MConst {
-	
-	/** The Constant CODE$access_token. */
 	public static final String CODE$access_token                     = "${code.access.token}";
-	
-	/** The Constant CODE$hash. */
 	public static final String CODE$hash                             = "${code.hash}";
-	
-	/** The Constant CODE$registry. */
 	public static final String CODE$registry                         = "${code.registry}";
-	
-	/** The Constant CODE$request. */
 	public static final String CODE$request                          = "${code.request}";
-	
-	/** The Constant DATE$transaction. */
 	public static final String DATE$transaction                      = "${date.transaction}";
-	
-	/** The Constant DESC$code_access_token. */
 	public static final String DESC$code_access_token                = "${desc.code.access.token}";
-	
-	/** The Constant DESC$code_hash. */
 	public static final String DESC$code_hash                        = "${desc.code.hash}";
-	
-	/** The Constant DESC$code_request. */
 	public static final String DESC$code_request                     = "${desc.code.request}";
-	
-	/** The Constant DESC$date_transaction. */
 	public static final String DESC$date_transaction                 = "${desc.date.transaction}";
-	
-	/** The Constant DESC$description_password. */
 	public static final String DESC$description_password             = "${desc.description.password}";
-	
-	/** The Constant DESC$dto_request_device_register. */
 	public static final String DESC$dto_request_device_register      = "${desc.dto.request.device.register}";
-	
-	/** The Constant DESC$identify_device. */
 	public static final String DESC$identify_device                  = "${desc.identify.device}";
-	
-	/** The Constant DESC$list_roles. */
 	public static final String DESC$list_roles                       = "${desc.list.roles}";
-	
-	/** The Constant DESC$mutation_auth_user. */
 	public static final String DESC$mutation_auth_user               = "${desc.mutation.auth.user}";
-	
-	/** The Constant DESC$mutation_confirm_register_device. */
 	public static final String DESC$mutation_confirm_register_device = "${desc.mutation.confirm.register.device}";
-	
-	/** The Constant DESC$name_username. */
 	public static final String DESC$name_username                    = "${desc.name.username}";
-	
-	/** The Constant DESC$subscription_device_register. */
 	public static final String DESC$subscription_device_register     = "${desc.subscription.device.register}";
-	
-	/** The Constant DESC$type_auth_user. */
 	public static final String DESC$type_auth_user                   = "${desc.type.auth.user}";
-	
-	/** The Constant DESCRIPTION$password. */
 	public static final String DESCRIPTION$password                  = "${description.password}";
-	
-	/** The Constant DTO$request_device_register. */
 	public static final String DTO$request_device_register           = "${dto.request.device.register}";
-	
-	/** The Constant ERROR$access_unauthorized. */
 	public static final String ERROR$access_unauthorized             = "${error.access.unauthorized}";
-	
-	/** The Constant ERROR$invalid_code_register. */
 	public static final String ERROR$invalid_code_register           = "${error.invalid.code.register}";
-	
-	/** The Constant IDENTIFY$device. */
 	public static final String IDENTIFY$device                       = "${identify.device}";
-	
-	/** The Constant LIST$roles. */
 	public static final String LIST$roles                            = "${list.roles}";
-	
-	/** The Constant MUTATION$auth_user. */
 	public static final String MUTATION$auth_user                    = "${mutation.auth.user}";
-	
-	/** The Constant MUTATION$confirm_register_device. */
 	public static final String MUTATION$confirm_register_device      = "${mutation.confirm.register.device}";
-	
-	/** The Constant NAME$username. */
 	public static final String NAME$username                         = "${name.username}";
-	
-	/** The Constant SUBSCRIPTION$device_register. */
 	public static final String SUBSCRIPTION$device_register          = "${subscription.device.register}";
-	
-	/** The Constant TYPE$auth_user. */
 	public static final String TYPE$auth_user                        = "${type.auth.user}";
-	
-	/** The Constant TYPE$registry. */
 	public static final String TYPE$registry                         = "${type.registry}";
 }
 
